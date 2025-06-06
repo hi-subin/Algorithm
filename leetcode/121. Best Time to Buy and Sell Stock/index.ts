627b22018322885e1eab4cbcef9c768f26b84fdc
subin <subinlee@nineright.com>
2025-06-06 11:57:11 +0900

    leetcode: Solve 121. Best Time to Buy and Sell Stock

---
+++ leetcode/121. Best Time to Buy and Sell Stock/index.ts
@@ -0,0 +1,16 @@
+const maxProfit = (prices: number[]): number => {
+    let buy = prices[0], profit = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] < buy) {
+            buy = prices[i];
+        }
+
+        const newProfit = prices[i] - buy;
+        if (newProfit > profit) {
+            profit = newProfit;
+        }
+    } 
+
+    return profit;
+};
